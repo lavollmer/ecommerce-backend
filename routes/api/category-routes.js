6ce07bcf7e6922baecafc614b2cbb0e2f5c46869
lavollmer <141365306+lavollmer@users.noreply.github.com>
2023-11-11 13:52:49 -0600

    debugging PUT request with POSTMAN

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -48,7 +48,9 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
   // update a category by its `id` value
   try {
-    const payload = await Category.update(req.body);
+    const payload = await Category.update(req.body, {
+      where: { id: req.params.id }
+    });
     res.status(200).json({ status: 'success', payload })
   } catch (err) {
     res.status(500).json({ status: 'error', sendback: err.message })
